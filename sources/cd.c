7a1a9ba734f36a883b3a655b359207b3a86c0b08
Theo Freydiere <tfreydie@z1r1p2.42lyon.fr>
2024-04-18 13:56:40 +0200

    will this make a pull request pretty please

--- sources/cd.c
+++ sources/cd.c
@@ -6,7 +6,7 @@
 /*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 17:54:37 by tfreydie          #+#    #+#             */
-/*   Updated: 2024/04/18 11:42:28 by tfreydie         ###   ########.fr       */
+/*   Updated: 2024/04/18 13:56:19 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,6 @@ int cd(char *directory_to_jump_to)
 {
     // t_test lalala = malloc(sizeof(t_test));
     // free(lalala);
+
+    //Added comment so that it does a fucking pull request reeeee
 }

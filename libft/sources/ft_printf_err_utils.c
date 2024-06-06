7c0c9f3eb9d48a1e9fd14c54587fc54f15fbafb9
Theo Freydiere <tfreydie@z4r6p2.42lyon.fr>
2024-06-06 18:38:08 +0200

    tout normer ya juste un for loop debugging

--- libft/sources/ft_printf_err_utils.c
+++ libft/sources/ft_printf_err_utils.c
@@ -6,16 +6,17 @@
 /*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 15:31:36 by tfreydie          #+#    #+#             */
-/*   Updated: 2024/04/23 15:45:29 by tfreydie         ###   ########.fr       */
+/*   Updated: 2024/06/06 17:47:22 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void			ft_recursive_print(unsigned long long ptr, int *size);
+static void			ft_recursive_print(unsigned long long ptr, int *size,
+						char buffer[BUFFER_SIZE]);
 static unsigned int	ft_base_length(char *base);
 
-void	ft_putnbr_err(int nbr, int *size)
+void	ft_putnbr_err(int nbr, int *size, char buffer[BUFFER_SIZE])
 {
 	long long	real_nbr;
 	int			digit;
@@ -24,57 +25,60 @@ void	ft_putnbr_err(int nbr, int *size)
 	if (real_nbr < 0 && *size != -1)
 	{
 		real_nbr *= -1;
-		ft_putncount_char_err('-', size);
+		ft_putncount_char_err('-', size, buffer);
 	}
 	if (real_nbr >= 10 && *size != -1)
 	{
-		ft_putnbr_err(real_nbr / 10, size);
-		ft_putnbr_err(real_nbr % 10, size);
+		ft_putnbr_err(real_nbr / 10, size, buffer);
+		ft_putnbr_err(real_nbr % 10, size, buffer);
 	}
 	else if (real_nbr < 10 && *size != -1)
 	{
 		digit = real_nbr + '0';
-		ft_putncount_char_err(digit, size);
+		ft_putncount_char_err(digit, size, buffer);
 	}
 }
 
-void	ft_print_ptr_err(unsigned long long ptr, int *size)
+void	ft_print_ptr_err(unsigned long long ptr, int *size,
+			char buffer[BUFFER_SIZE])
 {
 	if (ptr == 0)
-		ft_putncount_str_err("(nil)", size);
+		ft_putncount_str_err("(nil)", size, buffer);
 	else
 	{
-		ft_putncount_str_err("0x", size);
-		ft_recursive_print(ptr, size);
+		ft_putncount_str_err("0x", size, buffer);
+		ft_recursive_print(ptr, size, buffer);
 	}
 }
 
-static void	ft_recursive_print(unsigned long long ptr, int *size)
+static void	ft_recursive_print(unsigned long long ptr, int *size,
+				char buffer[BUFFER_SIZE])
 {
 	char	*base;
 
 	base = "0123456789abcdef";
 	if (ptr >= 16 && *size != -1)
 	{
-		ft_recursive_print(ptr / 16, size);
-		ft_recursive_print(ptr % 16, size);
+		ft_recursive_print(ptr / 16, size, buffer);
+		ft_recursive_print(ptr % 16, size, buffer);
 	}
 	else if (ptr < 16 && *size != -1)
-		ft_putncount_char_err(base[ptr], size);
+		ft_putncount_char_err(base[ptr], size, buffer);
 }
 
-void	ft_putuns_err(unsigned int nbr, char *base, int *size)
+void	uns_2(unsigned int nbr, char *base, int *size,
+			char buffer[BUFFER_SIZE])
 {
 	unsigned int		base_len;
 
 	base_len = ft_base_length(base);
 	if (nbr >= base_len && *size != -1)
 	{
-		ft_putuns_err(nbr / base_len, base, size);
-		ft_putuns_err(nbr % base_len, base, size);
+		uns_2(nbr / base_len, base, size, buffer);
+		uns_2(nbr % base_len, base, size, buffer);
 	}
 	else if (nbr < base_len && *size != -1)
-		ft_putncount_char_err(base[nbr], size);
+		ft_putncount_char_err(base[nbr], size, buffer);
 }
 
 static unsigned int	ft_base_length(char *base)
